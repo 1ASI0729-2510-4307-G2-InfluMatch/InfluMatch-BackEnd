130da2dec7270f1883ba96799fe5606c80ebf7f4
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-06-20 23:42:56 -0500

    feat: add coments TokenBlacklistService.java

--- src/main/java/com/influmatch/auth/application/service/TokenBlacklistService.java
+++ src/main/java/com/influmatch/auth/application/service/TokenBlacklistService.java
@@ -5,15 +5,20 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+
 @Service
 public class TokenBlacklistService {
+    
+    // Conjunto thread-safe para almacenar tokens que han sido puestos en la lista negra
     private final Set<String> blacklistedTokens = ConcurrentHashMap.newKeySet();
 
+    // Método para agregar un token a la lista negra
     public void blacklist(String token) {
-        blacklistedTokens.add(token);
+        blacklistedTokens.add(token); // Añade el token al conjunto de tokens bloqueados
     }
 
+    // Método para verificar si un token está en la lista negra
     public boolean isBlacklisted(String token) {
-        return blacklistedTokens.contains(token);
+        return blacklistedTokens.contains(token); // Devuelve verdadero si el token está en la lista negra
     }
-} 
+}
