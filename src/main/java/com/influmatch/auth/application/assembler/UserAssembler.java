822902fa720c64be30e9db2d8dee0d13ed82b5c7
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-06-20 23:42:11 -0500

    feat: add coments UserAssembler.java

--- src/main/java/com/influmatch/auth/application/assembler/UserAssembler.java
+++ src/main/java/com/influmatch/auth/application/assembler/UserAssembler.java
@@ -8,16 +8,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+// Definición del componente UserAssembler, utilizado para ensamblar datos relacionados con el usuario
 @Component
-@RequiredArgsConstructor
+@RequiredArgsConstructor  // Genera un constructor con parámetros para las dependencias finales (como passwordEncoder)
 public class UserAssembler {
+    
+    // Dependencia final para codificar contraseñas
     private final PasswordEncoder passwordEncoder;
 
+    // Método para convertir un objeto RegisterRequest en un objeto User (entidad)
     public User toEntity(RegisterRequest request) {
+        // Se crea una nueva instancia de User con los datos proporcionados en el request
         return new User(
-            new Email(request.getEmail()),
-            new Password(passwordEncoder.encode(request.getPassword())),
-            request.getRole()
+            new Email(request.getEmail()), // Crea un nuevo objeto Email a partir del correo del request
+            new Password(passwordEncoder.encode(request.getPassword())), // Codifica la contraseña antes de asignarla
+            request.getRole() // Asigna el rol proporcionado en el request
         );
     }
-} 
+}
